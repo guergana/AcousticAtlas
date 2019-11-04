df9b5ae3a201d4d42dae36f9f0b8fe1a3d25de92
Guergana <pestanias@yahoo.com>
2019-11-04 10:10:53 +0100

    initial commit

---
+++ scripts/controller.js
@@ -0,0 +1,94 @@
+// var target = new Nexus.Slider('#target',{
+    // 'size': [120,20],
+    // 'mode': 'relative',  // Hihgpass
+    // 'min': 0,
+    // 'max': 150,
+    // 'step': 1,
+    // 'value': 0
+// });
+
+var target2 = new Nexus.Slider('#target2',{
+    'size': [120,10],
+    'mode': 'relative',  // Dry wet
+    'min': 0,
+    'max': 1,
+    'step': 0.02,
+    'value': 1
+});
+
+target2.colorize("accent","#666");
+
+var toggle = new Nexus.Toggle('#toggle',{
+    'size': [60,10],
+    'state': false
+});
+
+toggle.colorize("accent","#666");
+
+var toggle2 = new Nexus.Toggle('#toggle2',{
+  'size': [60,10],
+  'state': false
+});
+
+toggle2.colorize("accent","#666");
+
+//////////////// ACTIONS ON INPUT
+
+// target.on('change',function(v) {
+    
+    // filt1.frequency.value = v;
+    // filterDisp.innerHTML = " Highpass filter " + v +"Hz";
+  // });
+
+  target2.on('change',function(v) {
+    
+    convolver.wet.value = v;
+    conv.innerHTML =" Dry/Wet " + Math.round(v*100) + "%";
+  });
+
+  toggle.on('change',function(v) {
+    console.log(v);
+    if(v){
+		player.start();
+        player.volume.setValueAtTime(-22, 2);
+        Ambient1.innerHTML = "Ambient s1 On";
+    } else{
+		player.stop();
+        player.volume.setValueAtTime(-Infinity, 4);
+        Ambient1.innerHTML = "Ambient 1 off";
+    }
+  });
+
+  toggle2.on('change',function(v) {
+    console.log(v);
+    if(v){
+		player2.start();
+        player2.volume.setValueAtTime(-22, 2);
+        Ambient2.innerHTML = "Ambient 2 On";
+    } else{
+		player2.stop();
+        player.volume.setValueAtTime(-Infinity, 4);
+        Ambient2.innerHTML = "Ambient 2 off";
+    }
+  });
+  
+  // const closeImageBtn = document.querySelector("#image-container .close");
+  // closeImageBtn.onclick = function(){
+	  // const container = document.getElementById("image-container");
+	  // container.style.display =  'none';
+  // };
+  
+  let fullscreenOn = false;
+  
+  let fullscreenButton = document.querySelector("#image-container .fullscreen");
+  fullscreenButton.addEventListener('click', function (event) { 
+	  if (!fullscreenOn) {
+		document.querySelector(".panel-right").requestFullscreen();
+		fullscreenOn = true;
+		} else {
+		document.exitFullscreen();
+		fullscreenOn = false;
+		}
+	}, false);
+	
+
---
+++ scripts/db-globe.js
@@ -0,0 +1,241 @@
+  // Initialize Cloud Firestore through Firebase
+  firebase.initializeApp({
+    apiKey: 'AIzaSyATlkbJJXn8NTNQmy50HrMtlRvX-g8Ivp0',
+    authDomain: 'pacoustic-atlas.firebaseapp.com',
+    projectId: 'acoustic-atlas',
+    //storageBucket: 'acoustic-atlas.appspot.com'
+  });
+ 
+     // Globe
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhMzE4YzhlMC0zYWMyLTQyNWItYmFkZi04MzJhYzEyNGUxODAiLCJpZCI6MTY5MDUsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzEzMDUzNTl9.H2-dtkBwj9ik_vvYrE0deVJKfEWaJXz0YkM0U1QCHss';
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+    scene3DOnly: true,
+    selectionIndicator: false,
+    baseLayerPicker: false,
+    creditsDisplay: false,
+    animation: false,
+    timeline: false,
+    homeButton: false,
+    navigationHelpButton: false,
+    infoBox : false,
+	fullscreenButton: false,
+	geocoder: false
+    /*terrainProvider : new Cesium.CesiumTerrainProvider({
+      url : Cesium.IonResource.fromAssetId(3956),
+      requestVertexNormals : true
+  })*/
+});
+    
+ function img_create(src, title) {
+	 var img = document.createElement('img');
+	 img.src = '/img/' + src;
+	 if ( title != null ) {img.alt = title;img.title = title;} 
+	 return img;
+}
+
+	var db = firebase.firestore();
+    var dataSourceForCluster = new Cesium.CustomDataSource('clusters');
+	var pinBuilder = new Cesium.PinBuilder();
+		
+    db.collection("locations").get().then((querySnapshot) => {
+	
+		querySnapshot.forEach((doc) => {
+			console.log('data doc.id', doc.id, doc.data());
+			// viewer.entities.add({
+				// name : doc.data().name,
+				// id: doc.id,
+				// description: '<img src="' + doc.data().image + '">',
+				// position : Cesium.Cartesian3.fromDegrees(doc.data().latitude, doc.data().longitude),
+				// billboard : {
+					// image : pinBuilder.fromText('📢', Cesium.Color.BLACK, 32).toDataURL(),
+					// verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+				// }
+			// });
+			dataSourceForCluster.entities.add({
+			   name : doc.data().name,
+				id: doc.id,
+				description: '<img src="' + doc.data().image + '">',
+				position : Cesium.Cartesian3.fromDegrees(doc.data().latitude, doc.data().longitude),
+				billboard : {
+					image : pinBuilder.fromText('📢', Cesium.Color.BLACK, 32).toDataURL(),
+					verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+				}
+			});
+		});
+	}); //end db
+
+var dataSourcePromise = viewer.dataSources.add(dataSourceForCluster);
+
+dataSourcePromise.then(function(dataSource) {
+	dataSource.clustering.pixelRange = 10;
+    dataSource.clustering.minimumClusterSize = 2;
+    dataSource.clustering.enabled = true;
+	
+	var removeListener;
+    var pin50 = pinBuilder.fromText('50+', Cesium.Color.RED, 48).toDataURL();
+    var pin40 = pinBuilder.fromText('40+', Cesium.Color.ORANGE, 48).toDataURL();
+    var pin30 = pinBuilder.fromText('30+', Cesium.Color.YELLOW, 48).toDataURL();
+    var pin20 = pinBuilder.fromText('20+', Cesium.Color.GREEN, 48).toDataURL();
+    var pin10 = pinBuilder.fromText('10+', Cesium.Color.BLUE, 48).toDataURL();
+
+    var singleDigitPins = new Array(8);
+    for (var i = 0; i < singleDigitPins.length; ++i) {
+        singleDigitPins[i] = pinBuilder.fromText('' + (i + 2), Cesium.Color.GRAY, 36).toDataURL();
+    }
+			console.log('outside customStyle');
+    function customStyle() {
+
+        if (Cesium.defined(removeListener)) {
+            removeListener();
+            removeListener = undefined;
+        } else {
+            removeListener = dataSource.clustering.clusterEvent.addEventListener(function(clusteredEntities, cluster) {
+                cluster.label.show = false;
+                cluster.billboard.show = true;
+                cluster.billboard.id = cluster.label.id;
+                cluster.billboard.verticalOrigin = Cesium.VerticalOrigin.BOTTOM;
+
+                if (clusteredEntities.length >= 50) {
+                    cluster.billboard.image = pin50;
+                } else if (clusteredEntities.length >= 40) {
+                    cluster.billboard.image = pin40;
+                } else if (clusteredEntities.length >= 30) {
+                    cluster.billboard.image = pin30;
+                } else if (clusteredEntities.length >= 20) {
+                    cluster.billboard.image = pin20;
+                } else if (clusteredEntities.length >= 10) {
+                    cluster.billboard.image = pin10;
+                } else {
+                    cluster.billboard.image = singleDigitPins[clusteredEntities.length - 2];
+                }
+            });
+        }
+
+        // force a re-cluster with the new styling
+        var pixelRange = dataSource.clustering.pixelRange;
+        dataSource.clustering.pixelRange = 0;
+        dataSource.clustering.pixelRange = pixelRange;
+    }
+
+    // start with custom style
+    customStyle();
+
+    var viewModel = {
+        pixelRange: pixelRange,
+        minimumClusterSize: minimumClusterSize
+    };
+    Cesium.knockout.track(viewModel);
+});
+//end of clusters
+
+   var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+    // If the mouse is over a point of interest, change the entity billboard scale and color
+    var previousPickedEntity = undefined;
+    handler.setInputAction(function(movement) {
+        var pickedPrimitive = viewer.scene.pick(movement.endPosition);
+        var pickedEntity = (Cesium.defined(pickedPrimitive)) ? pickedPrimitive.id : undefined;
+        // Unhighlight the previously picked entity
+        if (Cesium.defined(previousPickedEntity)) {
+            previousPickedEntity.billboard.scale = 1.0;
+            previousPickedEntity.billboard.color = Cesium.Color.WHITE;
+        }
+        // Highlight the currently picked entity
+        if (Cesium.defined(pickedEntity) && Cesium.defined(pickedEntity.billboard)) {
+            pickedEntity.billboard.scale = 1.5;
+            pickedEntity.billboard.color = Cesium.Color.YELLOW;
+            previousPickedEntity = pickedEntity;
+        }
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+    
+  handler.setInputAction(function(e) {
+      
+      var pickedPrimitive = viewer.scene.pick(e.position);
+      var pickedEntity = (Cesium.defined(pickedPrimitive)) ? pickedPrimitive.id : undefined;
+      var ellipsoid = viewer.scene.globe.ellipsoid;
+    if(pickedEntity && !Array.isArray(pickedEntity)){
+	//is not a cluster, it's a single pin
+    var cartesian = new Cesium.Cartesian3(pickedEntity.position._value.x, pickedEntity.position._value.y, pickedEntity.position._value.z );
+    if (cartesian) {
+        var cartographic = ellipsoid.cartesianToCartographic(cartesian);
+        var longitudeString = Cesium.Math.toDegrees(cartographic.longitude);
+        var latitudeString = Cesium.Math.toDegrees(cartographic.latitude);
+        //pickedEntity.position = cartesian;
+        var name = pickedEntity.name;
+		var id = pickedEntity.id;
+        var result = id + '- '+  name + ': '+ longitudeString + ', '+ latitudeString;  // We can reuse this 
+     
+		var docRef = db.collection("locations").doc(id);
+
+		docRef.get().then(function(doc) {
+			if (doc.exists) {
+				let imageContainer = document.getElementById("image-container");
+				let descriptionContainer = document.querySelectorAll("#description-container span")[0];
+				let imageSet = document.querySelectorAll("#image-container img")[0]; 
+				console.log('imageSet', imageSet);
+				let imageNew = img_create(doc.data().image, doc.data().name );
+				
+				descriptionContainer.innerHTML = doc.data().description;
+				imageSet.replaceWith(imageNew);
+				
+				imageContainer.style.display = 'block';
+				// if(imageContainer.children.length > 1){
+					// imageContainer.children[1].remove();
+				// }
+				//imageContainer.appendChild(image);
+				// let panelMain = document.getElementsByClassName("panel-main")[0];
+				// panelMain.appendChild(imageContainer);
+				loadIr('./ir/' + doc.data().IR_filename);
+				console.log("Document data:", doc.data());
+			} else {
+				// doc.data() will be undefined in this case
+				console.log("No such document!");
+			}
+		}).catch(function(error) {
+			console.log("Error getting document:", error);
+		});
+    }
+    } else {
+		//clicked on a cluster
+		  var pickedLabel = viewer.scene.pick(e.position);
+		  console.log('pickedLabel', pickedLabel);
+		if (Cesium.defined(pickedLabel)) {
+			var ids = pickedLabel.id;
+			if (Cesium.isArray(ids)) {
+				for (var i = 0; i < ids.length; ++i) {
+					ids[i].billboard.color = Cesium.Color.RED;
+				}
+			}
+		}
+	}
+      
+  }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+    
+    viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+    
+    /*viewer.entities.add({
+    id: 'mou',
+    label: {
+        // position : Cesium.Cartesian2.ZERO, 
+        show: true   // Removed semicolon here
+    }
+});
+    viewer.scene.canvas.addEventListener('mousemove', function(e) {
+    var entity = viewer.entities.getById('mou');
+    var ellipsoid = viewer.scene.globe.ellipsoid;
+    // Mouse over the globe to see the cartographic position 
+    var cartesian = viewer.camera.pickEllipsoid(new Cesium.Cartesian3(e.clientX, e.clientY), ellipsoid);
+    if (cartesian) {
+        var cartographic = ellipsoid.cartesianToCartographic(cartesian);
+        var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(10);
+        var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(10);
+        entity.position = cartesian;
+        entity.label.show = true;
+        entity.label.font_style = 84;
+        //entity.position= Cesium.Cartesian2.ZERO; 
+        entity.label.text = '(' + longitudeString + ', ' + latitudeString + ')';
+        var result = entity.label.text;  // We can reuse this
+        document.getElementById("demo").innerHTML = result;
+    } else {
+        entity.label.show = false;
+    }
+});*/
