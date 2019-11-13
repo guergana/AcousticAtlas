ac8bba4cb437a324d8ecd92a9391093a130d8858
Guergana <pestanias@yahoo.com>
2019-11-13 14:51:35 +0100

    test

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
+const sharp = require("sharp");
+const fs = require("fs");
+
 module.exports = (req, res) => {
   const { name = "World" } = req.query;
-  res.send(`Hello ${name}!`);
+  //res.send(`Hello ${name}!`);
+  const path = "../img/Bristol_Cathedral_Lady_Chapel_Bristol_UK_-_Diliff.jpg";
+  const readStream = fs.createReadStream(path);
+  let transform = sharp()
+    .toFormat("jpg")
+    .resize(1024, 860);
+  //res.send(readStream.pipe(transform));
+  return readStream.pipe(transform);
 };
