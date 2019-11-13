1f5abda9b2b56fcd45619da482a766072c8131d7
Guergana <pestanias@yahoo.com>
2019-11-13 15:14:00 +0100

    sharp test

--- api/index.js
+++ api/index.js
@@ -5,12 +5,15 @@ module.exports = (req, res) => {
   const { name = "World" } = req.query;
   //res.send(`Hello ${name}!`);
   const path = "../img/Bristol_Cathedral_Lady_Chapel_Bristol_UK_-_Diliff.jpg";
-  let transform = sharp(path)
+  sharp(path)
     .rotate()
     .resize(1024, 840)
     .toBuffer()
     .then(data => {
-      res.send(data);
+      fs.writeFileSync("../img/yellow.png", data);
+      res.writeHead(200, { "Content-Type": "image/jpeg" });
+      // stream the file
+      fs.createReadStream(filePath, "utf-8").pipe(res);
     })
     .catch(err => {
       res.send("error:", err);
