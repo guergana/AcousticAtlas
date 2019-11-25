4a7deeeeffda8e06cacdcf79120a1b3d223181d6
telepathicbeing <43606186+telepathicbeing@users.noreply.github.com>
2019-11-25 22:20:42 +0100

    Add files via upload

--- scripts/db-globe.js
+++ scripts/db-globe.js
@@ -214,11 +214,7 @@ handler.setInputAction(function(e) {
 
             imageContainer.style.display = "block";
             loadIr("./ir/" + doc.data().IR_filename);
-            loadAmbient1("./audios/"+ doc.data().Ambient1_filename);
-            loadAmbient2("./audios2/"+ doc.data().Ambient2_filename);
             console.log("Document data:", doc.data());
-
-
           } else {
             // doc.data() will be undefined in this case
             console.log("No such document!");
